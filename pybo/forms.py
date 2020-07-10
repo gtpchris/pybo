c009e98d17c1acd24aaef22f46678b43f78e868a
이종훈 <gtpchris@gmail.com>
2020-07-11 03:41:33 +0900

    파이보 프로젝트 최초 커밋

---
+++ pybo/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from pybo.models import Question, Answer, Comment
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = ['subject', 'content']
+        widgets = {
+            'subject': forms.TextInput(attrs={'class':'form-control'}),
+            'content': forms.Textarea(attrs={'class':'form-control', 'rows':10}),
+        }
+
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ['content']
+        labels = {
+            'content': '답변내용',
+        }
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+        labels = {
+            'content': '댓글내용',
+        }
