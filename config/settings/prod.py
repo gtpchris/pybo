d086640ce9c27676d17b99be1445cd85c83df2cc
leejonghun <gtpchris@gmail.com>
2020-07-13 01:08:21 +0900

    gglabs.kr 호스트 추가

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['15.164.255.161']
+ALLOWED_HOSTS = ['15.164.255.161','gglabs.kr']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = []
 DEBUG = False
