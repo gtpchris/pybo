aea71289655a3d93541f92ab9ed371285807abb9
leejonghun <gtpchris@gmail.com>
2020-07-12 18:47:59 +0900

    base_dir 수정2

--- config/settings/base.py
+++ config/settings/base.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from typing import Any, Union
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
