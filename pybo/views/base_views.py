1e623a0c0f28cacb0e251611731a37598ee20c28
leejonghun <gtpchris@gmail.com>
2020-07-13 00:27:12 +0900

    logger7

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -14,8 +14,8 @@ def index(request):
     pybo 목록 출력
     """
 
-    # 3/0  # 강제로 오류발생
-    logger.info("info 레벨 출력")
+     3/0  # 강제로 오류발생
+    # logger.info("info 레벨 출력")
     # 입력 파라미터
     page = request.GET.get('page','1')      # 페이지
     kw = request.GET.get('kw', '')          # 검색어
