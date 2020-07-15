47706ef441f83902ac1b8e90996c8adbff73e915
leejonghun <gtpchris@gmail.com>
2020-07-15 20:31:34 +0900

    tagging

--- config/settings/base.py
+++ config/settings/base.py
@@ -82,23 +82,25 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
+# 로컬 SQLite3 db
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'gglabsdb',
-        'USER': 'gglabsdb',
-        'PASSWORD': 'Dthinker21B!',
-        'HOST': 'pg1101.gabiadb.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# 가비아 DB서버
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'gglabsdb',
+#         'USER': 'gglabsdb',
+#         'PASSWORD': 'Dthinker21B!',
+#         'HOST': 'pg1101.gabiadb.com',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
