a312db839e0015c5ac798afadd0564f3802cbf8d
leejonghun <gtpchris@gmail.com>
2020-07-15 17:53:12 +0900

    requirements update  2  - postgresql

--- config/settings/base.py
+++ config/settings/base.py
@@ -79,13 +79,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'gglabsdb',
+        'USER': 'gglabsdb',
+        'PASSWORD': 'Dthinker21B!',
+        'HOST': 'pg1101.gabiadb.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
