697456cf03692b52f9ae9140db73fb6b8721f100
leejonghun <gtpchris@gmail.com>
2020-07-15 17:49:07 +0900

    requirements update  - postgresql

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,3 +4,14 @@ ALLOWED_HOSTS = ['15.164.255.161','gglabs.kr']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = []
 DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'gglabsdb',
+        'USER': 'gglabsdb',
+        'PASSWORD': 'Dthinker21B!',
+        'HOST': 'pg1101.gabiadb.com',
+        'PORT': '5432',
+    }
+}
