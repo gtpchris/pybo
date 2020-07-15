e2d9e3dd491bb98337350a444ba7e87fbf6c9e89
leejonghun <gtpchris@gmail.com>
2020-07-15 23:33:53 +0900

    chapter06

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import ListView, DetailView
 from django.views.generic import ArchiveIndexView, YearArchiveView, MonthArchiveView
 from django.views.generic import DayArchiveView, TodayArchiveView
+from django.views.generic import ListView, DetailView, TemplateView
 
 from blog.models import Post
 
@@ -9,7 +10,6 @@ logger = logging.getLogger(__name__)
 
 #--- ListView
 class PostLV(ListView):
-    logger.info("blog PostLV() 진입")
     model = Post
     template_name = 'blog/post_all.html'
     context_object_name = 'posts'
@@ -34,7 +34,6 @@ class PostYAV(YearArchiveView):
 
 
 class PostMAV(MonthArchiveView):
-    logger.info('PostMAV 진입1')
     model = Post
     date_field = 'modify_dt'
 
@@ -47,4 +46,22 @@ class PostDAV(DayArchiveView):
 class PostTAV(TodayArchiveView):
     model = Post
     date_field = 'modify_dt'
+
+
+#--- Tag View
+class TagCloudTV(TemplateView):
+    template_name = 'taggit/taggit_cloud.html'
+
+
+class TaggedObjectLV(ListView):
+    template_name = 'taggit/taggit_post_list.html'
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(tags__name=self.kwargs.get('tag'))
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tagname'] = self.kwargs['tag']
+        return context
 
