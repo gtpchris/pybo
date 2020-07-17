785b79edd9b5718e9a171095ae58d0b27025f898
이종훈 <gtpchris@gmail.com>
2020-07-17 15:52:16 +0000

    resist correct

--- pybo/views/vote_views.py
+++ pybo/views/vote_views.py
@@ -5,7 +5,7 @@ from django.shortcuts import get_object_or_404, redirect
 from ..models import Question, Answer
 
 
-@login_required(login_url='resistration:login')
+@login_required(login_url='registration:login')
 def vote_question(request, question_id):
     """
     pybo 질문추천등록
@@ -18,7 +18,7 @@ def vote_question(request, question_id):
     return redirect('pybo:detail', question_id=question.id)
 
 
-@login_required(login_url='resistration:login')
+@login_required(login_url='registration:login')
 def vote_answer(request, answer_id):
     """
     pybo 답글추천등록
